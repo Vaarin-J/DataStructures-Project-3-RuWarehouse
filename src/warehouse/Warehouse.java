7ba6c87cec32e63352ce63dbda4da17785a883c3
Vaarin <vaarinjain@gmail.com>
2023-08-06 12:39:29 -0400

    first commit

---
+++ src/warehouse/Warehouse.java
@@ -0,0 +1,186 @@
+package warehouse;
+
+/*
+ *
+ * This class implements a warehouse on a Hash Table like structure, 
+ * where each entry of the table stores a priority queue. 
+ * Due to your limited space, you are unable to simply rehash to get more space. 
+ * However, you can use your priority queue structure to delete less popular items 
+ * and keep the space constant.
+ * 
+ * @author Ishaan Ivaturi
+ */ 
+public class Warehouse {
+    private Sector[] sectors;
+    
+    // Initializes every sector to an empty sector
+    public Warehouse() {
+        sectors = new Sector[10];
+
+        for (int i = 0; i < 10; i++) {
+            sectors[i] = new Sector();
+        }
+    }
+    
+    /**
+     * Provided method, code the parts to add their behavior
+     * @param id The id of the item to add
+     * @param name The name of the item to add
+     * @param stock The stock of the item to add
+     * @param day The day of the item to add
+     * @param demand Initial demand of the item to add
+     */
+    public void addProduct(int id, String name, int stock, int day, int demand) {
+        evictIfNeeded(id);
+        addToEnd(id, name, stock, day, demand);
+        fixHeap(id);
+    }
+
+    /**
+     * Add a new product to the end of the correct sector
+     * Requires proper use of the .add() method in the Sector class
+     * @param id The id of the item to add
+     * @param name The name of the item to add
+     * @param stock The stock of the item to add
+     * @param day The day of the item to add
+     * @param demand Initial demand of the item to add
+     */
+    private void addToEnd(int id, String name, int stock, int day, int demand) {
+            //sector to add to is last digit of id
+            //initial date of last purchase of your new Product should be set to the current day (day).
+        int sec = id % 10;
+        Sector sector = sectors[sec];
+        sector.add(new Product(id, name, stock, day, demand));
+    }
+
+    /**
+     * Fix the heap structure of the sector, assuming the item was already added
+     * Requires proper use of the .swim() and .getSize() methods in the Sector class
+     * @param id The id of the item which was added
+     */
+    private void fixHeap(int id) {
+        // Popularity = Initial Demand + Total Amount Purchased + Date of Last Purchase
+        int sec = id % 10;
+        Sector sector = sectors[sec];
+        sector.swim(sector.getSize());
+        
+    }
+
+    /**
+     * Delete the least popular item in the correct sector, only if its size is 5 while maintaining heap
+     * Requires proper use of the .swap(), .deleteLast(), and .sink() methods in the Sector class
+     * @param id The id of the item which is about to be added
+     */
+    private void evictIfNeeded(int id) {
+        int sec = id % 10;
+        Sector sector = sectors[sec];        
+        if(sector.getSize()==5){
+            sector.swap(1, 5);
+            sector.deleteLast();
+            sector.sink(1);
+        }
+
+    }
+
+    /**
+     * Update the stock of some item by some amount
+     * Requires proper use of the .getSize() and .get() methods in the Sector class
+     * Requires proper use of the .updateStock() method in the Product class
+     * @param id The id of the item to restock
+     * @param amount The amount by which to update the stock
+     */
+    public void restockProduct(int id, int amount) {
+
+        int sec = id % 10;
+        Sector sector = sectors[sec];
+        int size = sector.getSize();
+
+        for(int i=0; i<=size; i++){
+            if(sector.get(i) !=null){
+                if(sector.get(i).getId() == id){
+                sector.get(i).updateStock(amount);
+                }
+        }
+    }
+
+    }
+    
+    /**
+     * Delete some arbitrary product while maintaining the heap structure in O(logn)
+     * Requires proper use of the .getSize(), .get(), .swap(), .deleteLast(), .sink() and/or .swim() methods
+     * Requires proper use of the .getId() method from the Product class
+     * @param id The id of the product to delete
+     */
+    public void deleteProduct(int id) {
+
+        int sec = id % 10;
+        Sector sector = sectors[sec];
+        int size = sector.getSize();
+
+       for(int i = 1; i <= size; i++){
+            Product temp = sector.get(i);
+            if(temp!=null && id == temp.getId() ){
+                sector.swap(i, size);
+                sector.deleteLast();
+                sector.sink(i);
+            }
+        }
+    }
+    /**
+     * Simulate a purchase order for some product
+     * Requires proper use of the getSize(), sink(), get() methods in the Sector class
+     * Requires proper use of the getId(), getStock(), setLastPurchaseDay(), updateStock(), updateDemand() methods
+     * @param id The id of the purchased product
+     * @param day The current day
+     * @param amount The amount purchased
+     */
+    public void purchaseProduct(int id, int day, int amount) {
+        int sec = id % 10;
+        Sector sector = sectors[sec];
+        int size = sector.getSize();
+
+        for(int i=1; i<=size; i++){
+            Product temp = sector.get(i);
+            if(id == temp.getId() && temp!=null && amount<=temp.getStock()){
+                temp.setLastPurchaseDay(day);
+                temp.updateStock((amount * -1));
+                temp.updateDemand((amount));
+                sector.sink(i);
+            }
+
+        }
+
+    }
+    
+    /**
+     * Construct a better scheme to add a product, where empty spaces are always filled
+     * @param id The id of the item to add
+     * @param name The name of the item to add
+     * @param stock The stock of the item to add
+     * @param day The day of the item to add
+     * @param demand Initial demand of the item to add
+     */
+    public void betterAddProduct(int id, String name, int stock, int day, int demand) {
+        // IMPLEMENT THIS METHOD
+    }
+
+    /*
+     * Returns the string representation of the warehouse
+     */
+    public String toString() {
+        String warehouseString = "[\n";
+
+        for (int i = 0; i < 10; i++) {
+            warehouseString += "\t" + sectors[i].toString() + "\n";
+        }
+        
+        return warehouseString + "]";
+    }
+
+    /*
+     * Do not remove this method, it is used by Autolab
+     */ 
+    public Sector[] getSectors () {
+        return sectors;
+    }
+}
